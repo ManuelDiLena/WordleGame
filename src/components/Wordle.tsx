801d3bcdf5ce48831cbfc0ade4d392ec24f40903
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-07-13 17:24:46 +0200

    Key detection with event

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -4,6 +4,36 @@ import RowCompleted from './RowCompleted';
 import RowCurrent from './RowCurrent';
 import RowEmpty from './RowEmpty';
 import { GameStatus } from './types';
+import { useWindow } from '../hooks/useWindow';
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+]
 
 export default function Wordle() {
 
@@ -13,15 +43,53 @@ export default function Wordle() {
     const [completedWords, setCompletedWords] = useState<string[]>([]);
     const [gameStatus, setGameStatus] = useState<GameStatus>(GameStatus.Playing);
 
+    useWindow('keydown', handleKeydown)
+
     useEffect(() => {
         setWordOfTheDay('break')
     })
+
+    function handleKeydown(event:KeyboardEvent) {
+        const letter = event.key.toUpperCase()
+
+        // Valid when deleted
+        if (event.key === 'Backspace' && currentWord.length > 0) {
+            onDelete()
+            return
+        }
+
+        if (event.key === 'Enter') {
+            return
+        }
+
+        if (currentWord.length >= 5) {
+            return
+        }
+
+        // Enter letter to state
+        if (keys.includes(letter)) {
+            onInput(letter)
+            return
+        }
+    }
+
+    // Function that writes the letters of the boxes
+    function onInput(letter:string) {
+        const newWord = currentWord + letter
+        setCurrentWord(newWord)
+    }
+
+    // Function that deletes the letters of the boxes
+    function onDelete() {
+        const newWord = currentWord.slice(0, -1)
+        setCurrentWord(newWord)
+    }
 
     return (
         <div>
            <RowCompleted word='sabio' solution={wordOfTheDay} />
            <RowCompleted word='sabio' solution={wordOfTheDay} />
-           <RowCurrent word='saba' />
+           <RowCurrent word={currentWord} />
            <RowEmpty />
            <RowEmpty />
            <RowEmpty />
