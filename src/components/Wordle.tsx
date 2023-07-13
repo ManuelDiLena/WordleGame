bd123f942d840883a08c0d8ee4c80aba0c9b9273
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-07-13 16:45:17 +0200

    Game state variables

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from 'react';
 import Box from './Box';
 import RowCompleted from './RowCompleted';
 import RowCurrent from './RowCurrent';
 import RowEmpty from './RowEmpty';
+import { GameStatus } from './types';
 
 export default function Wordle() {
+
+    const [wordOfTheDay, setWordOfTheDay] = useState<string>('');
+    const [turn, setTurn] = useState<number>(1);
+    const [currentWord, setCurrentWord] = useState<string>('');
+    const [completedWords, setCompletedWords] = useState<string[]>([]);
+    const [gameStatus, setGameStatus] = useState<GameStatus>(GameStatus.Playing);
+
+    useEffect(() => {
+        setWordOfTheDay('break')
+    })
+
     return (
         <div>
-           <RowCompleted word='sabio' solution='break' />
+           <RowCompleted word='sabio' solution={wordOfTheDay} />
+           <RowCompleted word='sabio' solution={wordOfTheDay} />
            <RowCurrent word='saba' />
            <RowEmpty />
            <RowEmpty />
